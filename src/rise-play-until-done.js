3019f186b602380d1346d087a794f97c1d713503
André Luiz Cardoso <andrecardoso@gmail.com>
2019-07-10 18:08:38 -0300

    Add PUD implementation

--- src/rise-play-until-done.js
+++ src/rise-play-until-done.js
@@ -1,6 +1,18 @@
 import { RiseElement } from "rise-common-component/src/rise-element.js";
+import { version } from "./rise-play-until-done-version.js";
 
 export default class RisePlayUntilDone extends RiseElement {
+
+  constructor() {
+    super();
+
+    this._setVersion( version );
+    this.setAttribute("play-until-done", true);
+  }
+
+  reportDone() {
+    super._sendEvent( "report-done", {done: true});
+  }
 
 }
 
